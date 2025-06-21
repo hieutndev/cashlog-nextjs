61d200babfd3bfcaa85162f2abecbee2705e72bd
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-06-21 14:32:24 +0700

    refactor: replace dropdown nav to heroui nav menu

--- components/shared/partials/horizontal-nav.tsx
+++ components/shared/partials/horizontal-nav.tsx
@@ -2,17 +2,20 @@
 
 import { Button } from "@heroui/button";
 import { useReactiveCookiesNext } from "cookies-next/client";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@heroui/dropdown";
 import { useRouter } from "next/navigation";
 import clsx from "clsx";
+import { useState } from "react";
+import { Navbar, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@heroui/navbar";
 
 import SYS_ICONS from "@/config/icons";
-import { SITE_CONFIG } from "@/config/site-config";
 import useScreenSize from "@/hooks/useScreenSize";
 import { BREAK_POINT } from "@/config/break-point";
+import { SITE_CONFIG } from "@/config/site-config";
 
 export default function HorizontalNav() {
 	const { getCookie, hasCookie, deleteCookie } = useReactiveCookiesNext();
+
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const router = useRouter();
 
@@ -31,7 +34,7 @@ export default function HorizontalNav() {
 					invisible: width > BREAK_POINT.L,
 				})}
 			>
-				<Dropdown size={"lg"}>
+				{/* <Dropdown size={"lg"}>
 					<DropdownTrigger>
 						<Button
 							startContent={SYS_ICONS.BARS.LG}
@@ -54,7 +57,33 @@ export default function HorizontalNav() {
 							</DropdownItem>
 						)}
 					</DropdownMenu>
-				</Dropdown>
+				</Dropdown> */}
+
+				<Navbar
+					isMenuOpen={isMenuOpen}
+					onMenuOpenChange={setIsMenuOpen}
+				>
+					<NavbarContent>
+						<NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+					</NavbarContent>
+
+					<NavbarMenu className={"mt-4"}>
+						{SITE_CONFIG.sidebarItems.map((item, index) => (
+							<NavbarMenuItem key={`${item}-${index}`}>
+								<Button
+									size={"lg"}
+									variant="light"
+									onPress={() => {
+										router.push(item.href);
+										setIsMenuOpen(false); // Close the menu after navigation
+									}}
+								>
+									{item.label}
+								</Button>
+							</NavbarMenuItem>
+						))}
+					</NavbarMenu>
+				</Navbar>
 			</div>
 			{hasCookie("refresh_token") ? (
 				<div className={"flex items-center gap-2"}>
